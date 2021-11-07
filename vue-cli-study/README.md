61b3fd04a70b2fd4b97d557e62460d1496a9f4f3
jiwonsoong <jiwonsoong13@gmail.com>
2021-11-08 03:00:31 +0900

    Create new directory

---
+++ vue-cli-study/README.md
@@ -0,0 +1,27 @@
+# vue-cli
+
+## Overview
+싱글 파일 컴포넌트에 배운 내용을 적용하여 개발 시작하기 
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue-cli-study/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
+  </div>
+  <!-- template 안에는 root가 하나만 있어야 함! div가 병렬적으로 두 개 올 수 없음. -->
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
+  components: {
+    'app-header': AppHeader,
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
